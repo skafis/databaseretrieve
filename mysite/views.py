8f7d652e9a051335fa86732e6c0f626af5632598
FrankGwada <franciskipchumba5@gmail.com>
2016-04-06 15:31:07 +0000

    make query to order by the latest

--- mysite/views.py
+++ mysite/views.py
@@ -4,12 +4,12 @@ from .models import View
 from .forms import PostForm
 # Create your views here.
 def show_list(request):
-    list = View.objects.all()
+    list = View.objects.order_by('-created_date')
     
     form = PostForm( request.POST or None)
     if form.is_valid():
         instance = form.save(commit=False)
-        print form.cleaned_data.get("message")
+        # print form.cleaned_data.get("message")
         instance.save()
     # if request.method == "POST":
     #     print request.POST.get("name")
@@ -22,3 +22,9 @@ def show_list(request):
     # list = print list1
     return render(request, 'mysite/index.html',context)
     #return HttpResponse(print(list.as_ul()))
+    
+def detail_view(request):
+    context = {
+            "object_list":list
+            }
+    return render(request, 'mysite/detail_view.html', context)
